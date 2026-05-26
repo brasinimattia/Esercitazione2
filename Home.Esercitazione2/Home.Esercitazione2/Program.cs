d1ed128e83cd51a200a548868908809c0811a51a
brasinimattia <mattia.brasini.stud@ispascalcomandini.it>
2026-05-26 23:29:20 +0200

    FIxed

--- Home.Esercitazione2/Home.Esercitazione2/Program.cs
+++ Home.Esercitazione2/Home.Esercitazione2/Program.cs
@@ -4,15 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Creazione guidata della Biblioteca
         Console.WriteLine("=== CONFIGURAZIONE BIBLIOTECA ===");
         Console.Write("Inserisci il nome della biblioteca: ");
         string Library = Console.ReadLine();
         Console.Write("Inserisci l'indirizzo: ");
         string Adress = Console.ReadLine();
-        Console.Write("Orario Apertura (es. 09:00): ");
+        Console.Write("Orario Apertura: ");
         string Opening = Console.ReadLine();
-        Console.Write("Orario Chiusura (es. 18:00): ");
+        Console.Write("Orario Chiusura: ");
         string Closing = Console.ReadLine();
 
         Library MyLibrary = new Library(Library, Adress, Opening, Closing);
@@ -41,16 +40,15 @@ class Program
                     Console.Write("Numero di Pagine: ");
                     int pages = int.Parse(Console.ReadLine());
 
-                    // Istanziamo il libro usando il costruttore della tua classe Book
                     Book newbook = new Book(author, title, year, editor, pages);
 
-                    // Lo aggiungiamo alla biblioteca
+
                     MyLibrary.AddBook(newbook);
                     break;
 
                 case "2":
                     IsRunning = false;
-                    Console.WriteLine("Chiusura del programma di test.");
+                    Console.WriteLine("Chiusura del programma.");
                     break;
 
                 default:
