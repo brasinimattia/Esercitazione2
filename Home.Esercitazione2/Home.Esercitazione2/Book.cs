0d30926ba3d0a0c28b082560250fe1a220a3d3da
brasinimattia <mattia.brasini.stud@ispascalcomandini.it>
2026-05-26 22:22:13 +0200

    Book class done

--- Home.Esercitazione2/Home.Esercitazione2/Book.cs
+++ Home.Esercitazione2/Home.Esercitazione2/Book.cs
@@ -8,5 +8,40 @@ namespace Home.Esercitazione2
 {
     public class Book
     {
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public int PublicationYear { get; set; }
+
+        public string Publisher { get; set; }
+
+        public int NumberOfPages { get; set; }
+
+
+        public Book(string author, string title, int publicationYear, string publisher, int numberOfPages)
+        {
+            Author = author;
+            Title = title;
+            PublicationYear = publicationYear;
+            Publisher = publisher;
+            NumberOfPages = numberOfPages;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: '{Title}', Author: {Author}, Year: {PublicationYear}, Publisher: {Publisher}, Pages: {NumberOfPages}";
+        }
+
+        public string ReadingTime()
+        {
+            if (NumberOfPages < 100)
+                return "1h";
+            if (NumberOfPages >= 100 && NumberOfPages <= 200)
+                return "2h";
+
+            return ">2h";
+        }
     }
 }
