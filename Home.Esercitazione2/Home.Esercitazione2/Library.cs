e1c4f85742f44ef9b276dccd3aebdc3607d66ae3
brasinimattia <mattia.brasini.stud@ispascalcomandini.it>
2026-05-26 22:25:21 +0200

    Library class done

--- Home.Esercitazione2/Home.Esercitazione2/Library.cs
+++ Home.Esercitazione2/Home.Esercitazione2/Library.cs
@@ -8,5 +8,56 @@ namespace Home.Esercitazione2
 {
     public class Library
     {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string OpeningHour { get; set; }
+        public string ClosingHour { get; set; }
+
+        private List<Book> Books;
+
+        public Library(string name, string adress, string openinghour, string closinghour)
+        {
+            Name = name;
+            Adress = adress;
+            OpeningHour = openinghour;
+            ClosingHour = closinghour;
+            Books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            if (book != null)
+                Books.Add(book);
+
+        }
+
+
+        public int BooksNumber()
+        {
+            return Books.Count;
+        }
+
+        public Book SearchByTitle(string title)
+        {
+            foreach (Book l in Books)
+            {
+                if (l.Title == title)
+                    return l;
+            }
+            return null;
+        }
+
+
+        public List<Book> SearchByAuthor(string author)
+        {
+            List<Book> result = new List<Book>();
+            foreach (Book l in Books)
+            {
+                if (l.Author == author)
+                    result.Add(l);
+            }
+            return result;
+        }
+
     }
 }
